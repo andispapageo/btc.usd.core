55c52b045595a4358e9675ad6a04a2651ff05e2a
andispapageo <a.papageorgiou@techbrand.io>
2022-11-28 01:54:40 +0200

    btcusd.core - adding gitignore

--- btc.usd.core/Program.cs
+++ btc.usd.core/Program.cs
@@ -1,3 +1,5 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddControllersWithViews();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var engine = builder.Services.ConfigureApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
