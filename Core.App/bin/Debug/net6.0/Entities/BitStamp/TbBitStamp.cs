7ac37491f6987a2660932089dd48699a5ee3bea5
andispapageo <a.papageorgiou@techbrand.io>
2022-11-29 20:57:20 +0200

    btcusd.core - mapping attributes

---
+++ Core.App/bin/Debug/net6.0/Entities/BitStamp/TbBitStamp.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Entities.BitStamp
+{
+    public class TbBitStamp
+    {
+        public int Id { get; set; }
+        public long timestamp { get; set; }
+        public decimal close { get; set; }
+        public decimal high { get; set; }
+        public decimal low { get; set; }
+        public decimal last { get; set; }
+        public decimal volume { get; set; }
+        public decimal vwap { get; set; }
+        public decimal bid { get; set; }
+        public decimal ask { get; set; }
+        public decimal open_24 { get; set; }
+        public decimal percent_change_24 { get; set; }
+        public TbBitStamp() { }
+    }
+}
